ea71dda919fe94bdad6c861c2adb67fe27179635
li <li230316@163.com>
2023-06-15 10:15:24 +0800

    202306151015

--- C_STUN/C_STUN/main.c
+++ C_STUN/C_STUN/main.c
@@ -6,6 +6,11 @@
 //
 
 #include <stdio.h>
+#include <EndpointSecurity/EndpointSecurity.h>
+
+void hanle(es_message_t* msg,void* p) {
+    *(uint32_t*)p= msg->version;
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
