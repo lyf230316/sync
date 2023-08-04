9630422379b3f2c97491740d63aa0baea5aef66f
v_liyufeng04 <v_liyufeng04@baidu.com>
2023-08-04 19:38:01 +0800

    1937

--- HashStorage/HashStorage/Storage.swift
+++ HashStorage/HashStorage/Storage.swift
@@ -10,14 +10,16 @@ import Foundation
 
 
 class Storage {
-    
-    
-    public class func upload(wd:String, file: String,key: String) {
+    public class func upload(wd:String, file: String,keyPath: String,repo:String,branch:String,commit: String) -> Bool {
         guard prepareGitDir(wd: wd) else {
-            return
+            return false
         }
-        try! FileManager.default.copyItem(atPath: file, toPath: wd)
-        
+        let name = (file as NSString).lastPathComponent;
+        try! FileManager.default.copyItem(atPath: file, toPath: wd+"/"+name)
+        guard commitAndPush(wd: wd, repo: repo, keyPath: keyPath, branch: branch, commit: commit) else {
+            return false
+        }
+        return true
     }
     
     class func prepareGitDir(wd: String) -> Bool {
@@ -27,9 +29,6 @@ class Storage {
         try! FileManager.default.createDirectory(atPath: wd, withIntermediateDirectories: true)
         
         let task = Process()
-//        task.environment = [
-//            "GIT_SSH_COMMAND": String(format: "/usr/bin/ssh -i %@", key)
-//        ]
         task.executableURL = URL(string: "file:///bin/bash")
         task.arguments = [
             "-c",
@@ -45,10 +44,10 @@ class Storage {
         return true
     }
     
-    class func commitAndPush(wd: String,repo: String, key: String) {
+    class func commitAndPush(wd: String,repo: String, keyPath: String, branch:String, commit:String) -> Bool {
         let task = Process()
         task.environment = [
-            "GIT_SSH_COMMAND": String(format: "/usr/bin/ssh -i %@", key)
+            "GIT_SSH_COMMAND": String(format: "/usr/bin/ssh -i %@", keyPath)
         ]
         task.executableURL = URL(string: "file:///bin/bash")
         task.arguments = [
@@ -56,11 +55,20 @@ class Storage {
             String(format: """
 /usr/bin/cd %@
 /usr/bin/git add --all
-/usr/bin/git commit -m 
-""", wd)
+/usr/bin/git commit -m "%@"
+/usr/bin/git  branch -M "%@"
+/usr/bin/git remote add origin "%@"
+/usr/bin/git push -u origin "%@"
+""", wd,commit, branch, repo, branch)
         ]
         task.launch()
         task.waitUntilExit()
+        let status = task.terminationStatus
+        guard status == 0 else {
+            print(task.terminationReason)
+            return false
+        }
+        return true
     }
 }
 
