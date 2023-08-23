890c4add4b62b2eab3629666685ef7e794237d41
v_liyufeng04 <v_liyufeng04@baidu.com>
2023-08-23 09:42:16 +0800

    08230942

--- CPTest/CPTest/NSPasteboard+Crypto.m
+++ CPTest/CPTest/NSPasteboard+Crypto.m
@@ -100,8 +100,11 @@
 +(NSData*)data_md5:(NSData *)data {
     const char *cStr = data.bytes;
     unsigned char digest[CC_MD5_DIGEST_LENGTH] = {0};
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
     CC_MD5( cStr, (CC_LONG)data.length, digest );
     NSData *dig = [NSData dataWithBytes:digest length:CC_MD5_DIGEST_LENGTH];
+#pragma clang diagnostic pop
 //    NSLog(@"md5:%@ --> %@",data.debugDescription,dig.debugDescription);
     return dig;
 }
--- CPTest/CPTest/ViewController.m
+++ CPTest/CPTest/ViewController.m
@@ -7,7 +7,6 @@
 
 #import "ViewController.h"
 #import <WebKit/WebKit.h>
-#import "Util.h"
  
 @interface ViewController ()<WKUIDelegate, WKNavigationDelegate>
 
@@ -34,33 +33,6 @@
     // Update the view, if already loaded.
 }
 - (IBAction)export:(id)sender {
-    NSMutableDictionary * plist = [NSMutableDictionary dictionary];
-    NSMutableSet *set = [Util globalArray];
-    for (NSPasteboard *board in set) {
-        NSMutableArray *items = [NSMutableArray array];
-        for (NSPasteboardItem *item in board.pasteboardItems) {
-            NSMutableDictionary *types = [NSMutableDictionary dictionary];
-            for (NSString *type in item.types) {
-                NSMutableDictionary *dic = [NSMutableDictionary dictionary];
-                NSData *data = [item dataForType:type];
-                if (data) {
-                    dic[@"data"] = data;
-                }
-                NSString *str = [item stringForType:type];
-                if (str) {
-                    dic[@"string"] = str;
-                }
-                id pl = [item propertyListForType:type];
-                if (pl) {
-                    dic[@"propertyList"] = pl;
-                }
-                types[type] = dic;
-            }
-            [items addObject:types];
-        }
-        plist[board.name] = items;
-    }
-    [plist writeToFile:@"/Users/lyf/git/github/sync/CPTest/CPTest/pasteboard.plist" atomically:YES];
 }
 
 @end
